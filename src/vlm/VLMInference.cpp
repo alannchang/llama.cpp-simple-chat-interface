82be02169ae7b0bc246b9bd8fb338ac0ec3759f8
Shubham <noreply+shubham@objectbox.io>
2025-02-24 20:20:35 +0530

    sync with llama.cpp upstream

---
+++ src/vlm/VLMInference.cpp
@@ -0,0 +1,5 @@
+//
+// Created by equip on 21-02-2025.
+//
+
+#include "VLMInference.h"
---
+++ src/vlm/VLMInference.h
@@ -0,0 +1,12 @@
+#ifndef VLMINFERENCE_H
+#define VLMINFERENCE_H
+
+#include "llama.h"
+
+class VLMInference {
+
+};
+
+
+
+#endif //VLMINFERENCE_H
